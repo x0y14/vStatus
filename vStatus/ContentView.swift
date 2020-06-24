1b7f2985f2d8a8fc16f7585466f5d6fe97e85094
x0y14 <ya01ma08@gmail.com>
2020-06-24 17:43:23 +0900

    Initial Commit

---
+++ vStatus/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  vStatus
+//
+//  Created by yuhei yamauchi on 2020/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ vStatus/vStatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  vStatusApp.swift
+//  vStatus
+//
+//  Created by yuhei yamauchi on 2020/06/24.
+//
+
+import SwiftUI
+
+@main
+struct vStatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
